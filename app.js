d5aca22041e861632269617d041495bda4282640
kawre <69250723+kawre@users.noreply.github.com>
2020-09-24 15:19:21 +0200

    q&a

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mobileNav = document.querySelector(".mobile-nav");
 const headerMobile = document.querySelector(".header-mobile");
 const hamImg = document.querySelector(".ham-img");
 const headerLogo = document.querySelector(".header-logo");
+const featuresBtn = document.querySelectorAll(".features-btn");
+const featuresUnderline = document.querySelectorAll(".button-underline");
 
 hamBtn.addEventListener("click", function () {
   mobileNav.classList.toggle("show-navbar");
@@ -20,3 +22,22 @@ function changeImage() {
     headerLogo.src = "/images/logo-bookmark.svg";
   }
 }
+
+featuresBtn.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    const parent = e.currentTarget.parentElement.children[1];
+    parent.classList.add("show-underline");
+    console.log(e.currentTarget.parentElement);
+  });
+});
+
+/* Q&A */
+
+const question = document.querySelectorAll(".question");
+
+question.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const questionContainer = e.currentTarget.parentElement;
+    questionContainer.classList.toggle("show-text");
+  });
+});
